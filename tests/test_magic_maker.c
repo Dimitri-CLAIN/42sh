cf3d3d1d9132bbbcc4d043d2d4fd7153d5789752
fabien <fabien.cheung-lim-yen@epitech.eu>
2019-05-24 17:24:49 +0400

    Added unit tests for the magic maker

--- tests/test_magic_maker.c
+++ tests/test_magic_maker.c
@@ -12,11 +12,11 @@
 Test(get_command, get_a_normal_command)
 {
     char *test1 = get_command(my_strdup("`ls`", KEEP));
-    cr_assert_str_eq(res, "ls");
+    cr_assert_str_eq(test1, "ls");
     char *test2 = get_command(my_strdup("``", KEEP));
-    cr_assert_str_eq(sauce, "\0");
+    cr_assert_str_eq(test2, "\0");
     char *test3 = get_command(my_strdup("` `", KEEP));
-    cr_assert_str_eq(barbecue, "\0");
+    cr_assert_str_eq(test3, "\0");
 }
 
 Test(magic_checker, check_a_few_strings)
@@ -25,5 +25,3 @@ Test(magic_checker, check_a_few_strings)
     cr_assert_eq(1, magic_checker(my_strdup("hello", KEEP)));
     cr_assert_eq(0, magic_checker(my_strdup("`hello`", KEEP)));
 }
-
-Test(tab_to_str)
